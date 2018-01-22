85d19ede8873cf982d425bd601694537d1b0383b
Lorito <mtprieto@uc.cl>
2018-01-22 11:47:40 -0300

    Only product view are shown and variant redirect

--- src/product/content/ProductView.jsx
+++ src/product/content/ProductView.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Redirect } from 'react-router'
 import { withStyles } from 'material-ui/styles';
 import Paper from 'material-ui/Paper';
 import Grid from 'material-ui/Grid';
@@ -75,42 +76,46 @@ class ProductView extends React.Component {
     else
       variantHtml = <div style={{width: '100%'}}><VariantSelector variants={variantNames}/></div>;
 
-    return (
-      <MuiThemeProvider theme={theme}>
-        <Header product={product}/>
-        <div className={classes.root}>
-          <Grid container spacing={24}>
-            <Grid item xs={12} sm={5}>
-              <Card className={classes.card}>
-                <CardMedia
-                  className={classes.media}
-                  image={product.image}
-                  title={product.name}
-                />
-              </Card>
+    if (product.variant.is_master)
+      return (
+        <MuiThemeProvider theme={theme}>
+          <Header product={product}/>
+          <div className={classes.root}>
+            <Grid container spacing={24}>
+              <Grid item xs={12} sm={5}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.media}
+                    image={product.image}
+                    title={product.name}
+                  />
+                </Card>
+              </Grid>
+              <Grid item xs={12} sm={7}>
+                {variantHtml}
+                <div style={{width: '100%'}}>
+                  <QuantitySelector />
+                </div>
+                <div style={{width: '100%'}}>
+                  <Button raised className={classes.button}>
+                    Add to Cart
+                </Button>
+                </div>
+                <div style={{width: '100%'}}>
+                  <Paper className={classes.description} elevation={4}>
+                    <Typography component="p">
+                      Paper can be used to build surface or other elements for your application. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </Typography>
+                  </Paper>
+                </div>
+              </Grid>
             </Grid>
-            <Grid item xs={12} sm={7}>
-              {variantHtml}
-              <div style={{width: '100%'}}>
-                <QuantitySelector />
-              </div>
-              <div style={{width: '100%'}}>
-                <Button raised className={classes.button}>
-                  Add to Cart
-              </Button>
-              </div>
-              <div style={{width: '100%'}}>
-                <Paper className={classes.description} elevation={4}>
-                  <Typography component="p">
-                    Paper can be used to build surface or other elements for your application. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                  </Typography>
-                </Paper>
-              </div>
-            </Grid>
-          </Grid>
-        </div>
-      </MuiThemeProvider>
-    );
+          </div>
+        </MuiThemeProvider>
+      );
+    else{
+      return (<Redirect to="/nomatch"/>);
+    }
   }
 }
 
