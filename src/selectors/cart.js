40d11b42a07c3d11af3e464f58dff7392d696df2
Lorito <mtprieto@uc.cl>
2018-01-23 12:25:59 -0300

    Hotfix

--- src/selectors/cart.js
+++ src/selectors/cart.js
@@ -1,8 +1,8 @@
 export const cartSelector = (state) => state.cart;
 
-export const getSubtotal = (state) =>{
+export const getSubtotal = (state) => {
   let count = 0;
-  state.cart.map(cartItem =>{
+  state.cart.forEach(cartItem =>{
     count = count + (cartItem.price*cartItem.quantity)
   });
   return count;
