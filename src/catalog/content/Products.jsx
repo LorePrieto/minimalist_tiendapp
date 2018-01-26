45494ef8515e4fd9dafe73d928f09f030c40e845
Lorito <mtprieto@uc.cl>
2018-01-26 17:53:38 -0300

    Catalog search product and category

--- src/catalog/content/Products.jsx
+++ src/catalog/content/Products.jsx
@@ -4,6 +4,9 @@ import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import Grid from 'material-ui/Grid';
 import { Link } from 'react-router-dom';
+import Paper from 'material-ui/Paper';
+import Typography from 'material-ui/Typography';
+import IntegrationAutosuggest from './IntegrationAutosuggest.jsx';
 
 // Redux
 import {connect} from 'react-redux';
@@ -14,22 +17,75 @@ const styles = theme => ({
   root: {
     marginTop: 'none',
   },
+  header: theme.mixins.gutters({
+    paddingTop: 16,
+    paddingBottom: 16,
+    marginBottom: theme.spacing.unit * 3,
+    boxShadow: 'none',
+    textAlign: 'center',
+  }),
+  typography: {
+    color: 'rgba(0,0,0,0.54)'
+  },
 });
 
 class Products extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: '',
+    };
+    this.onChangeProduct =  this.onChangeProduct.bind(this);
+    this.emptyQuery = this.emptyQuery.bind(this);
+  }
+
+  onChangeProduct = newQuery => event => {
+    this.setState({
+      query: newQuery
+    });
+  };
+
+  emptyQuery = () => {
+    this.setState({
+      query: ''
+    });
+  }
+
   render () {
-    const { classes } = this.props;
+    const { classes, products } = this.props;
+
+    let filteredProducts;
+    if (this.state.query === ''){
+      filteredProducts = products;
+    }else {
+      filteredProducts = products.filter(product => {
+        return product.name === this.state.query
+      });
+      if (filteredProducts.length === 0) {
+        filteredProducts = products.filter(product => product.taxon_names.some(taxon => taxon === this.state.query));
+      }
+    }
 
     return (
-      <Grid container spacing={40} className={classes.grid}>
-        {this.props.products.map(product => (
-          <Grid item key={product.name+product.id} xs={12} md={4}>
-            <Link to={'/product/'+product.variant_id} style={{textDecoration: 'none'}}>
-              <SimpleMediaCard data= {product} key={product.id} />
-            </Link>
-          </Grid>
-        ))}
-      </Grid>
+      <div>
+        <Paper className={classes.header} elevation={4}>
+          <Typography type="headline" component="h3" className={classes.typography}>
+            Catálogo
+          </Typography>
+          <div>
+            <IntegrationAutosuggest onChangeProduct={this.onChangeProduct} emptyQuery={this.emptyQuery} />
+          </div>
+        </Paper>
+        <Grid container spacing={40} className={classes.grid}>
+          {filteredProducts.map(product => (
+            <Grid item key={product.name+product.id} xs={12} md={4}>
+              <Link to={'/product/'+product.variant_id} style={{textDecoration: 'none'}}>
+                <SimpleMediaCard data= {product} key={product.id} />
+              </Link>
+            </Grid>
+          ))}
+        </Grid>
+      </div>
     );
   }
 }
